1ea9767f19ef4a6e1220caf24c3adae44c1003e4
DevelopersOfTalent <appsisteme.team@gmail.com>
2016-05-20 14:16:34 +0200

    Inicio notificaciones no terminado

--- app/src/main/java/com/example/arranque1/appsisteme/MainActivity.java
+++ app/src/main/java/com/example/arranque1/appsisteme/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.arranque1.appsisteme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.arranque1.appsisteme.bbdd.DaoContacts;
+import com.onesignal.OneSignal;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     EditText etPhone,etName;
@@ -18,8 +24,39 @@ public class MainActivity extends AppCompatActivity {
         etName = (EditText)findViewById(R.id.etName);
         etPhone = (EditText)findViewById(R.id.etPhone);
 
+
+        OneSignal.startInit(this)
+                .setAutoPromptLocation(true)
+                .setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
+                    @Override
+                    public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+                        //OneSignal.sendTag("key", "value");
+                        try {
+                            String casa = additionalData.get("key").toString();
+                            Log.d("hola",casa);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }})
+                .init();
+
+
+
     }
+
+
 
     public void iniciar(View v) {startActivity(new Intent(this, AddActivity.class));}
     public void ver (View v) {startActivity(new Intent(this,ListContactActivity.class));}
+
+    public void notificar (View v) {
+        OneSignal.getTags(new OneSignal.GetTagsHandler() {
+            @Override
+            public void tagsAvailable(JSONObject tags) {
+                Log.d("debug", tags.toString());
+
+            }
+        });
+    }
+
 }
